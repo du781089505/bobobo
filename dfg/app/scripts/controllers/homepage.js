c1960bc4a803713d4dd5451bf2205e9f6fcb44ed
DuJiang <781089505@qq.com>
2017-04-24 10:35:12 +0800

    嘻嘻嘻

--- dfg/app/scripts/controllers/homepage.js
+++ dfg/app/scripts/controllers/homepage.js
@@ -10,5 +10,30 @@
  */
   angular.module('myApp')
 .controller('homeCtrl', function($scope,$http,$state){
-	
+	//获取文章列表
+	$scope.arr =[]
+	$http({
+			url:"http://192.168.43.245:1235/homepage/list",
+			method:"get"
+		}).then(function(data){
+			console.log(data.data)
+			$scope.arr = data.data
+			
+		})
+		//登陆
+		$scope.denglu = function(){
+			console.log("1")
+			$http({
+			url:"http://192.168.43.245:1235/homepage/login",
+			method:"post",
+			data:{
+				username:$scope.username,
+				password:$scope.password
+			}
+		}).then(function(data){
+			console.log(1)
+			
+			
+		})
+		}
 })
--- jkl/routes/homepage.js
+++ jkl/routes/homepage.js
@@ -14,7 +14,7 @@ var pool = mysql.createPool({
 
          //获取列表信息
 router.get('/list',function(req,res){
-       console.log("login")
+//     console.log("login")
 	getuserName(function(err,rest){
 		if(err){
 			res.send(err)
@@ -42,5 +42,93 @@ function getuserName(callback){
 }
     //登录
     
+router.post('/login',function(req,res){
+       console.log("login-----")
+	var username = req.body.username;
+	var password = req.body.password
+
+	getLogin(username,function(err,rest){
+		if(rest.length==0){
+			res.send({flag:2})     //账号不存在
+		}else if(rest.length>0){
+			if(password ==rest[0].password){
+				res.send({flag:1})  //登陆成功
+			}else if(password !=rest[0].password){
+				res.send({flag:3})   //密码不正确
+			}
+		}else{
+			res.send({flag:4})
+		}
+		
+	})
+	
+})
+function getLogin(uname,callback){
+	pool.getConnection(function(err,conn){ //获取连接
+		var sql ='select * from user where username =?';
+		conn.query(sql,[uname],function(err,result){
+//			console.log(result)
+			if(err){
+//				console.log("getAllUser Eroor:"+err.message)
+				return
+			}
+			conn.release()  //释放连接
+			callback(err,result)
+		})
+	})
+}
+
+
+//注册
+
+router.post('/zhuce',function(req,res){
+
+	var username = req.body.username;
+	var password = req.body.password
+	var tel = req.body.tel
+	var email = req.body.email
+	
+	getuserZhce(username,function(err,rest){
+		if(rest ==""||rest == null){
+			save(username,password,tel,qq,name,age,function(err,resl){
+				console.log('dddd'+resl)
+				res.send({flag:1})  //注册成功
+			})
+		}else if(rest!=''||rest!=null){
+			res.send({flag:2})   //用户名已存在
+		}else{
+			res.send({flag:3})    //注册失败
+		}
+	})
+	
+})
+function getuserZhce(uname,callback){
+	pool.getConnection(function(err,conn){ //获取连接
+		var sql ='select * from user where username =?';
+		conn.query(sql,[uname],function(err,result){
+//			console.log(result)
+			if(err){
+//				console.log("getAllUser Eroor:"+err.message)
+				return
+			}
+			conn.release()  //释放连接
+			callback(err,result)
+		})
+	})
+}
+function save(name,pad,tel,qq,name,age,callback){
+	pool.getConnection(function(err,conn){ //获取连接
+		var sql ='insert into user(username,password,tel,qq,name,age) value (?,?,?,?,?,?)';
+		conn.query(sql,[name,pad,tel,qq,name,age],function(err,result){
+			console.log(result)
+			if(err){
+//				console.log("insertUser Eroor:"+err.message)
+				return
+			}
+			conn.release()  //释放连接
+			callback(err,result)
+		})
+	})
+}
 
 module.exports = router;
