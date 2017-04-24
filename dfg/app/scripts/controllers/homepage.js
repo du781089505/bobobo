eb47ed13eecce3c549408cbab4369ec765d75159
1997jhk <1072361042@qq.com>
2017-04-24 11:25:06 +0800

    111111111111111

--- dfg/app/scripts/controllers/homepage.js
+++ dfg/app/scripts/controllers/homepage.js
@@ -11,7 +11,22 @@
   angular.module('myApp')
 .controller('homeCtrl', function($scope,$http,$state){
 	//获取文章列表
+	$scope.x = true;
+	$scope.a = false;
+	$scope.e ="1";
 	$scope.arr =[]
+	$scope.t =function(){
+		if($scope.e =="1"){
+			$scope.x = false;
+	       $scope.a = true;
+	       $scope.e ="2";
+		}else if($scope.e =="2"){
+			$scope.x = true;
+	       $scope.a = false;
+	       $scope.e ="1";
+		}
+		
+	}
 	$http({
 			url:"http://192.168.43.245:1235/homepage/list",
 			method:"get"
@@ -20,7 +35,7 @@
 			$scope.arr = data.data
 			
 		})
-		//登陆
+//		登陆
 		$scope.denglu = function(){
 			console.log("1")
 			$http({
