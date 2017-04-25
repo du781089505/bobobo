7f6163128502f2d99eeea9bcec5daeb6f43e3d4e
马辉 <1014646524@qq.com>
2017-04-25 11:28:14 +0800

    wuhu

--- dfg/app/scripts/controllers/homepage.js
+++ dfg/app/scripts/controllers/homepage.js
@@ -8,35 +8,6 @@
  *
  * Main module of the application.
  */
-angular.module('myApp')
-	.controller('homeCtrl', function($scope, $http, $state) {
-		//获取文章列表
-		$scope.x = true;
-		$scope.a = false;
-		$scope.e = "1";
-		$scope.arr = []
-		$scope.arr1 = []
-		$scope.tishi = false;
-		$scope.tishi1 = false;
-		$scope.tishi2 = false;
-		$scope.t = function() {
-			if($scope.e == "1") {
-				$http({
-					url: "http://" + ip + "/homepage/cha",
-					method: "get"
-				}).then(function(data) {
-						console.log(data.data)
-					$scope.arr1=data.data	
-
-				})
-				$scope.x = false;
-				$scope.a = true;
-				$scope.e = "2";
-			} else if($scope.e == "2") {
-				$scope.x = true;
-				$scope.a = false;
-				$scope.e = "1";
-			}
   angular.module('myApp')
 .controller('homeCtrl', function($scope,$http,$state){
 	//获取文章列表
@@ -49,51 +20,42 @@ angular.module('myApp')
 	$scope.tishi2 = false;
 	$scope.zhuce1 = false;
 	$scope.zhuce2 = false;
+	$scope.queding = function(){
+		$scope.zhuce2 = false;
+	}
 	$scope.t =function(){
 		if($scope.e =="1"){
 			$scope.x = false;
 	       $scope.a = true;
 	       $scope.e ="2";
+	          $http({
+					url: "http://" + ip + "/homepage/cha",
+					method: "get"
+				}).then(function(data) {
+						console.log(data.data)
+					$scope.arr1=data.data	
+
+				})
 		}else if($scope.e =="2"){
 			$scope.x = true;
 	       $scope.a = false;
 	       $scope.e ="1";
 		}
-		$http({
-				url: "http://" + ip + "/homepage/list",
-				method: "get"
-			}).then(function(data) {
-				//			console.log(data.data)
-				$scope.arr = data.data
-
-			})
-			//		登陆
-		$scope.denglu = function() {
+		
+	}
+	$http({
+			url:"http://"+ip+"/homepage/list",
+			method:"get"
+		}).then(function(data){
+//			console.log(data.data)
+			$scope.arr = data.data
+			
+		})
+//		登陆
+		$scope.denglu = function(){
 			console.log("1")
-			console.log($scope.username, $scope.password)
+			console.log($scope.username,$scope.password)
 			$http({
-				url: "http://" + ip + "/homepage/login",
-				method: "post",
-				data: "username=" + $scope.username + "&password=" + $scope.password,
-				headers: {
-					'Content-Type': 'application/x-www-form-urlencoded'
-				}
-			}).then(function(data) {
-				if(data.data.flag == 1) {
-					//		  	$scope.tishi = true;
-					angular.element('.denglu').css({
-						"display": "block"
-					});
-				} else if(data.data.flag == 2) {
-					$scope.tishi1 = true;
-				} else if(data.data.flag == 3) {
-					$scope.tishi2 = true;
-				}
-
-			})
-		}
-
-	})
 			url:"http://"+ip+"/homepage/login",
 			method:"post",
 			data:"username="+$scope.username+"&password="+$scope.password,
