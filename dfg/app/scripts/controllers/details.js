802bf81ba633352cf4296b78759498939eb2d26e
马辉 <1014646524@qq.com>
2017-04-25 19:23:24 +0800

    22222222

--- dfg/app/scripts/controllers/details.js
+++ dfg/app/scripts/controllers/details.js
@@ -9,3 +9,15 @@
  * Main module of the application.
  */
   angular.module('myApp')
+  .controller('detailsCtrl', function($scope,$http,$state){
+  	 $scope.zai = true;
+  	 $scope.buzai = false;
+  	 $scope.detailscomment = function(){
+  	 	$scope.zai = false;
+  	 	$scope.buzai = true;
+  	 }
+  	 $scope.us_panel_post2 = function(){
+  	 	$scope.zai = true;
+  	 	$scope.buzai = false;
+  	 }
+  })
