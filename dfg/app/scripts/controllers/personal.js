ab7b945b587201b9a6c581f7fef487d552de2acb
马辉 <1014646524@qq.com>
2017-05-02 11:23:03 +0800

    woqu

--- dfg/app/scripts/controllers/personal.js
+++ dfg/app/scripts/controllers/personal.js
@@ -9,3 +9,6 @@
  * Main module of the application.
  */
   angular.module('myApp')
+  .controller('personal',['$scope','$timeout','$http','$rootScope','$stateParams','$state'], function($scope,$timeout, $http, $rootScope, $stateParams, $state){
+  	
+  })
