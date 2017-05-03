9049a17a190e3a4778085aa44fc364e3a0daa54f
DuJiang <781089505@qq.com>
2017-05-03 08:52:14 +0800

    eee

--- dfg/app/scripts/controllers/personal.js
+++ dfg/app/scripts/controllers/personal.js
@@ -9,4 +9,20 @@
  * Main module of the application.
  */
   angular.module('myApp')
+  .controller('personal', function($scope,$http,$state){
+  	$http({
+			url:"http://"+ip+"/personal/xun",
+			method:"get",
+			params:{username:localStorage.username},
+			headers:{
+				'Content-Type': 'application/x-www-form-urlencoded'
+			}
+			
+		}).then(function(data){
+		    console.log(data)
+		    console.log(1111)
+		    
+		})
+  	
+  })
  
